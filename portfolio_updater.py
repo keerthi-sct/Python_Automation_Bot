1f0b6a900283e93ff9f7e327feb317f6636d0ea0
Keerthi Sreekumar <sreekumarkeerthi2007@gmail.com>
2026-06-13 13:10:28 +0530

    Added GitHub portfolio updater automation

---
+++ portfolio_updater.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+username = "keerthi-sct"
+
+url = f"https://api.github.com/users/{username}/repos"
+
+response = requests.get(url)
+
+repos = response.json()
+
+projects = []
+
+for repo in repos:
+    projects.append({
+        "name": repo["name"],
+        "url": repo["html_url"]
+    })
+
+with open("projects.json", "w") as file:
+    json.dump(projects, file, indent=4)
+
+print("Projects updated successfully!")
+print(projects)
